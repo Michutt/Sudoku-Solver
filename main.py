9a6d4fa779d795c831b4366ecf0122b02e21f516
Michutt <niemc.michal@gmail.com>
2020-06-02 20:15:22 +0200

    Add digit detection and selection

--- main.py
+++ main.py
@@ -3,14 +3,37 @@ import numpy as np
 
 
 def img_preparation(img):
+    # kernel = np.ones((10,10), np.uint8)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, img = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY)
+    img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
     return img
 
+def digits_detection(img):
+    output = cv2.connectedComponentsWithStats(img)
+    stats = output[2]
+    mask = []
+
+    stats = np.delete(stats, 0, 0)
+
+    for i in range(len(stats)):
+        if stats[i, cv2.CC_STAT_AREA] > img.shape[1] * img.shape[1] / 140:
+            mask.append(True)
+        else:
+            mask.append(False)
+
+    mask = np.where(mask)
+
+    return stats[mask]
+
+
 
 img_in = cv2.imread("img.jpg")
+
 img = img_preparation(img_in)
+stats = digits_detection(img)
+
 
 cv2.imshow('bin', img)
 cv2.waitKey(0)
